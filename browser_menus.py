4e1e8fbbbce0cdf7fb09d48d933783125b573772
Ren Tatsumoto <tatsu@autistici.org>
2022-10-20 12:47:55 +0300

    add grade now buttons

--- browser_menus.py
+++ browser_menus.py
@@ -1,11 +1,13 @@
 from aqt import gui_hooks
 from aqt.browser import Browser
 
+from .grade_now import add_grade_now_buttons
 from .learn_now import add_learn_now_button
 
 
 def on_browser_menus_did_init(self: Browser) -> None:
     add_learn_now_button(self)
+    add_grade_now_buttons(self)
 
 
 def init():
---
+++ grade_now.py
@@ -0,0 +1,61 @@
+# Learn Now add-on for Anki 2.1
+# Copyright: Ren Tatsumoto <tatsu at autistici.org>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+import functools
+from typing import Sequence, Dict, Literal
+
+from anki.cards import Card
+from anki.collection import Collection, OpChanges
+from aqt.browser import Browser
+from aqt.operations import CollectionOp
+from aqt.qt import *
+
+from .config import config
+from .learn_now import notify_user, with_undo_entry, get_selected_cards
+
+Ease = Literal[1, 2, 3, 4]
+
+
+@with_undo_entry(undo_msg="Grade cards from Browser")
+def grade_cards(col: Collection, cards: Sequence[Card], ease: Ease) -> OpChanges:
+    for card in cards:
+        card.start_timer()
+        col.sched.answerCard(card, ease)
+    # save the cards and add an undo entry.
+    return col.update_cards(cards)
+
+
+def is_not_suspended_or_buried(card: Card) -> bool:
+    # https://github.com/ankidroid/Anki-Android/wiki/Database-Structure
+    return card.queue >= 0
+
+
+def on_grade_cards(self: Browser, ease: Ease) -> None:
+    selected_cards = list(get_selected_cards(self))
+    to_answer = list(filter(is_not_suspended_or_buried, selected_cards))
+
+    CollectionOp(
+        parent=self, op=lambda col: grade_cards(col, to_answer, ease)
+    ).success(
+        lambda out: notify_user(f"Answered {len(to_answer)} out of {len(selected_cards)} cards.")
+    ).run_in_background()
+
+
+def answer_buttons() -> Dict[Ease, str]:
+    return {
+        1: 'Again',
+        2: 'Hard',
+        3: 'Good',
+        4: 'Easy',
+    }
+
+
+def add_grade_now_buttons(self: Browser):
+    grade_menu = self.form.menu_Cards.addMenu("Grade now")
+    for ease, text in answer_buttons().items():
+        action = grade_menu.addAction(text)
+        qconnect(action.triggered, functools.partial(on_grade_cards, self=self, ease=ease))
+        if shortcut := config.get(f'grade_{ease}'):
+            action.setShortcut(QKeySequence(shortcut))
+            action.setText(f"{action.text()} ({shortcut})")
