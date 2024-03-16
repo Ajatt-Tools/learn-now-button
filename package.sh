72c52e48ccc3cb3b8a6c0a97abbcef307144aead
Ren Tatsumoto <tatsu@autistici.org>
2024-03-16 03:53:30 +0300

    update package and ajt common

--- package.sh
+++ package.sh
@@ -1,18 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-readonly ADDON_NAME=learn_now
-readonly ROOT_DIR=$(git rev-parse --show-toplevel)
-readonly BRANCH=$(git branch --show-current)
-readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
-
-cd -- "$ROOT_DIR" || exit 1
-
-export ROOT_DIR BRANCH
-
-git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
-
-# shellcheck disable=SC2016
-git submodule foreach 'git archive main --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
-
-zipmerge "$ZIP_NAME" ./*.zip
-rm -- ./*.zip
+./ajt_common/package.sh \
+	--package "LearnNow" \
+	--name "AJT Learn Now" \
+	"$@"
