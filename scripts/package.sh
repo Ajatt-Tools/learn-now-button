aecb2010c0b2b8c68dd4892fe743169b9f871247
Ren Tatsumoto <tatsu@autistici.org>
2025-10-15 00:43:27 +0300

    update package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./ajt_common/package.sh \
+set -euo pipefail
+
+./card_management/ajt_common/package.sh \
 	--package "Card Management" \
 	--name "AJT Card Management" \
+	--root "card_management" \
 	"$@"
